c4204e4ad0f08957014fc4c97f6c2a03fd273ac7
Daniel Ipince <ipince22@gmail.com>
2021-03-02 07:11:06 +0000

    BASIC

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 export function Home() {
+	const [task, setTask] = useState("");
+	const [list, setList] = useState([]);
+
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container mt-5 text-center">
+			<div className="row d-flex justify-content-center">
+				<div className="col-md-8">
+					<div className="card">
+						<div className="card-body text-primary">
+							<h1 className="display-5">To Do List</h1>
+							<input
+								className="form-control"
+								type="text"
+								value={task}
+								onChange={e => {
+									setTask(e.target.value.toUpperCase());
+								}}
+								onKeyPress={e => {
+									if (e.key === "Enter") {
+										if (task != "") {
+											setList(
+												list.concat(task.toUpperCase())
+											);
+											setTask("");
+											console.log({ list });
+										} else {
+											alert("Ingrese una actividad");
+										}
+									}
+								}}
+							/>
+							{!list
+								? "loading..."
+								: list.map((item, index) => {
+										return (
+											<label
+												className="form-control text-primary list-group-item list-group-item-action"
+												key={index}
+												onDoubleClick={() => {
+													setList(
+														list.filter(
+															(itemf, indexf) =>
+																indexf !== index
+														)
+													);
+												}}>
+												{item}
+											</label>
+										);
+								  })}
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 }
