d5769392aefa3d69f111323708d2726a2005aaec
Daniel Ipince <ipince22@gmail.com>
2021-03-04 00:09:14 +0000

    samples

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -9,56 +9,8 @@ export function Home() {
 	const [list, setList] = useState([]);
 
 	return (
-		<div className="container mt-5 text-center">
-			<div className="row d-flex justify-content-center">
-				<div className="col-md-8">
-					<div className="card">
-						<div className="card-body text-primary">
-							<h1 className="display-5">To Do List</h1>
-							<input
-								className="form-control"
-								type="text"
-								value={task}
-								onChange={e => {
-									setTask(e.target.value.toUpperCase());
-								}}
-								onKeyPress={e => {
-									if (e.key === "Enter") {
-										if (task != "") {
-											setList(
-												list.concat(task.toUpperCase())
-											);
-											setTask("");
-											console.log({ list });
-										} else {
-											alert("Ingrese una actividad");
-										}
-									}
-								}}
-							/>
-							{!list
-								? "loading..."
-								: list.map((item, index) => {
-										return (
-											<label
-												className="form-control text-primary list-group-item list-group-item-action"
-												key={index}
-												onDoubleClick={() => {
-													setList(
-														list.filter(
-															(itemf, indexf) =>
-																indexf !== index
-														)
-													);
-												}}>
-												{item}
-											</label>
-										);
-								  })}
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+        <div className="container mt-5 text-center">
+             
+        </div>
+    );
 }
