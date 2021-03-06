9f2c0bc6c675596cc8edb8734a847880a2955321
Daniel Ipince <ipince22@gmail.com>
2021-03-06 03:14:12 +0000

    useEffect

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
@@ -7,10 +7,61 @@ import rigoImage from "../../img/rigo-baby.jpg";
 export function Home() {
 	const [task, setTask] = useState("");
 	const [list, setList] = useState([]);
+	useEffect(() => {
+		console.log({ list });
+	}, [list]);
 
 	return (
-        <div className="container mt-5 text-center">
-             
-        </div>
-    );
+		<div className="container mt-5 text-center">
+			<div className="row d-flex justify-content-center">
+				<div className="col-md-8">
+					<div className="card">
+						<div className="card-body text-primary">
+							<h1 className="display-5">To Do List</h1>
+							<input
+								className="form-control"
+								type="text"
+								value={task}
+								onChange={e => {
+									setTask(e.target.value.toUpperCase());
+								}}
+								onKeyPress={e => {
+									if (e.key === "Enter") {
+										if (task != "") {
+											setList(
+												list.concat(task.toUpperCase())
+											);
+											setTask("");
+											//console.log({ list });
+										} else {
+											alert("Ingrese una actividad");
+										}
+									}
+								}}
+							/>
+							{!list
+								? "loading..."
+								: list.map((item, index) => {
+										return (
+											<label
+												className="form-control text-primary list-group-item list-group-item-action"
+												key={index}
+												onDoubleClick={() => {
+													setList(
+														list.filter(
+															(itemf, indexf) =>
+																indexf !== index
+														)
+													);
+												}}>
+												{item}
+											</label>
+										);
+								  })}
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
